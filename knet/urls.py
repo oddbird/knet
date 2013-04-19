b2d9319e36f95cdc3a51c13aceedf9754fe69e94
Jonny Gerig Meyer <jonny@oddbird.net>
2013-04-19 11:26:38 -0400

    add basic demo content template/images

--- knet/urls.py
+++ knet/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     url(r'^$', 'knet.views.home', name='home'),
+    url(r'^demo/$', 'knet.views.demo', name='demo'),
 )
--- knet/views.py
+++ knet/views.py
@@ -2,3 +2,6 @@ from django.template.response import TemplateResponse
 
 def home(request):
     return TemplateResponse(request, 'home.html')
+
+def demo(request):
+    return TemplateResponse(request, 'demo.html')
