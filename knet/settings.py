e39982972f43af166068d532747168db76a3d0f1
Carl Meyer <carl@oddbird.net>
2013-04-19 18:40:01 -0600

    Add GZipMiddleware.

--- knet/settings.py
+++ knet/settings.py
@@ -89,6 +89,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE_CLASSES = (
+    'django.middleware.gzip.GZipMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
