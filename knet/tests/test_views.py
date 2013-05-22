df69865410cc461581b77e1d232d42654ae3a685
Carl Meyer <carl@oddbird.net>
2013-05-22 12:38:34 -0600

    Add messages display.

--- knet/tests/test_views.py
+++ knet/tests/test_views.py
@@ -1,6 +1,21 @@
+from knet.landing.models import Lead
+
+
 def test_landing(client):
     """Landing page loads without error."""
     client.get('/', status=200)
+
+
+def test_submit_lead(client):
+    """Can submit a lead."""
+    form = client.get('/').forms[0]
+    form['email'] = 'some@example.com'
+    resp = form.submit().follow()
+
+    resp.mustcontain('Thanks for your interest')
+    leads = list(Lead.objects.all())
+    assert len(leads) == 1
+    assert leads[0].email == 'some@example.com'
 
 
 def test_demo(client):
