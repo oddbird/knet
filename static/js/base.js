76c47cbe4b4663abe4d00b222e18df595c2fd7de
Jonny Gerig Meyer <jonny@oddbird.net>
2013-05-23 11:48:13 -0400

    add ajax-form, with django-messages-ui

--- static/js/base.js
+++ static/js/base.js
@@ -19,6 +19,20 @@ var KNET = (function (KNET, $) {
         DOWN: 40
     };
 
+    KNET.landingForm = function (formSel) {
+        var form = $(formSel);
+        if (form.length) {
+            form.ajaxForm({
+                resetForm: true
+                // success: function (response, status, xhr, form) {
+                //     // console.log('success');
+                // },
+                // target: form,
+                // replaceTarget: true
+            });
+        }
+    };
+
     return KNET;
 
 }(KNET || {}, jQuery));
