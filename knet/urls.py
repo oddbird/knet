0ce241d7d5901d3fb7cd4109492dff67fdfa4ce3
Eric Meyer <eric@oddbird.net>
2013-05-30 11:54:34 -0600

    Add teacher_detail template

--- knet/urls.py
+++ knet/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url, include
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 
 admin.autodiscover()
 
@@ -7,5 +8,9 @@ urlpatterns = patterns(
     '',
     url(r'^$', 'knet.landing.views.landing', name='landing'),
     url(r'^demo/$', 'knet.views.demo', name='demo'),
+    url(r'^teacher/(?P<teacher_id>\d+)/$',
+        TemplateView.as_view(template_name='teacher_detail.html'),
+        name='teacher_detail',
+        ),
     url(r'^admin/', include(admin.site.urls)),
 )
