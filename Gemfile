16d23068dfc6c00673912ddfc32b63c0288fad4c
Eric Meyer <eric@oddbird.net>
2013-06-03 15:27:22 -0600

    Setup styleguide

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source "http://rubygems.org"
 
 gem "sass", "~> 3.2.7"
 gem "compass", :git => "git://github.com/chriseppstein/compass", :branch => "master"
-gem "susy", :git => "git://github.com/ericam/susy", :branch => "master"
+gem "susy", :git => "git://github.com/ericam/susy", :branch => "susy-next"
 
 gem "accoutrement", :git => "git://github.com/ericam/accoutrement", :branch => "master"
 gem "modular-scale", "~> 1.0.6"
--- knet/urls.py
+++ knet/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url, include
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 
 admin.autodiscover()
 
@@ -10,6 +11,10 @@ urlpatterns = patterns(
     url(r'^teacher/(?P<username>\w+)/$',
         'knet.teachers.views.teacher_detail',
         name='teacher_detail',
+        ),
+    url(r'^styleguide/$',
+        TemplateView.as_view(template_name='styleguide/styleguide.html'),
+        name='styleguide',
         ),
     url(r'^admin/', include(admin.site.urls)),
 )
