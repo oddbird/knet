560476cb057d7adcce8188eabe8e0120f55e2757
Carl Meyer <carl@oddbird.net>
2013-06-04 14:14:02 -0600

    Use explicit imports in urls.py.

--- knet/urls.py
+++ knet/urls.py
@@ -2,16 +2,17 @@ from django.conf.urls import patterns, url, include
 from django.contrib import admin
 from django.views.generic.base import TemplateView
 
+from .landing.views import landing
+from .teachers.views import teacher_detail
+from .views import demo
+
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^$', 'knet.landing.views.landing', name='landing'),
-    url(r'^demo/$', 'knet.views.demo', name='demo'),
-    url(r'^teacher/(?P<username>\w+)/$',
-        'knet.teachers.views.teacher_detail',
-        name='teacher_detail',
-        ),
+    url(r'^$', landing, name='landing'),
+    url(r'^demo/$', demo, name='demo'),
+    url(r'^teacher/(?P<username>\w+)/$', teacher_detail, name='teacher_detail'),
     url(r'^styleguide/$',
         TemplateView.as_view(template_name='styleguide/styleguide.html'),
         name='styleguide',
