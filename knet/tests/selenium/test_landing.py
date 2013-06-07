1eeba07d7cd8db6b8cf006f131e2b324cfc50af2
Jonny Gerig Meyer <jonny@oddbird.net>
2013-06-07 17:00:22 -0400

    add selenium test for landing email form

--- knet/tests/selenium/test_landing.py
+++ knet/tests/selenium/test_landing.py
@@ -1,8 +1,14 @@
 """Selenium tests for landing page."""
-"""Selenium tests for landing page."""
+from selenium.webdriver.common.by import By
+
+from .pages import landing
 
 
-def test_landing_loads(selenium):
-    selenium.get(selenium.live_server.url)
 
-    assert selenium.title == "K Network"
+def test_user_can_submit_email_address(selenium):
+    landing_pg = landing.LandingPage(selenium)
+
+    landing_pg.go_to_landing_page()
+    landing_pg.signup()
+
+    assert landing_pg.is_element_present(By.CSS_SELECTOR, '#messages .success')
