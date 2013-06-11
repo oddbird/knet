f2c4eaac7ab3654beaa259a80c4bc3062dfa402c
Carl Meyer <carl@oddbird.net>
2013-06-11 11:58:50 -0600

    Don't link to nonexistent teacher profiles.

---
+++ knet/tests/teachers/templatetags/test_teachers.py
@@ -0,0 +1,17 @@
+from knet.teachers.templatetags.teachers import teacher_profile
+
+from ...factories import UserFactory
+from ..factories import TeacherProfileFactory
+
+
+class TestTeacherProfile(object):
+    def test_has_teacher_profile(self, db):
+        tp = TeacherProfileFactory.create()
+
+        assert teacher_profile(tp.user) == tp
+
+
+    def test_has_no_teacher_profile(self, db):
+        u = UserFactory.create()
+
+        assert teacher_profile(u) is None
