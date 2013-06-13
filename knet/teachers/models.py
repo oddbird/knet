6b04a667bda99042946036ab306b9350c36943c2
Carl Meyer <carl@oddbird.net>
2013-06-13 13:20:46 -0600

    Add some TODO items.

--- knet/teachers/models.py
+++ knet/teachers/models.py
@@ -24,6 +24,7 @@ class Story(models.Model):
     body = models.TextField()
 
     # this is always the user who actually created the story
+    # @@@ should be required, not null/blank=True
     submitter = models.ForeignKey(
         User, blank=True, null=True, related_name='submitted_stories')
     # overrides for stories posted by proxy to own profile
