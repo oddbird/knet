be7de5c03fe8106b7832683d6511b4dbeebd709f
Carl Meyer <carl@oddbird.net>
2013-06-15 13:41:54 -0600

    Remove landing page lead-submission code.

--- knet/landing/views.py
+++ knet/landing/views.py
@@ -1,32 +1,8 @@
 """Landing page views."""
-from django.contrib import messages
-from django.db import transaction
-from django.shortcuts import redirect
 from django.template.response import TemplateResponse
 
-from .decorators import ajax
-from .forms import LeadForm
 
 
-
-@ajax("landing/_form.html")
 def landing(request):
-    """A landing page with an email address signup."""
-    if request.method == "POST":
-        form = LeadForm(request.POST)
-        if form.is_valid():
-            with transaction.atomic():
-                form.save()
-            messages.success(
-                request, "Thanks for your interest; we'll be in touch soon!")
-            if not request.is_ajax():
-                return redirect("landing")
-        else:
-            # provide form errors as user messages instead of form errors
-            for fieldname, errors in form.errors.items():
-                for error in errors:
-                    messages.error(request, error)
-    else:
-        form = LeadForm()
-
-    return TemplateResponse(request, "home.html", {"form": form})
+    """A static landing page."""
+    return TemplateResponse(request, "home.html")
