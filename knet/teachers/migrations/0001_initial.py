d67a66f26a3f931b1cd05868fe3070a44e4af689
Carl Meyer <carl@oddbird.net>
2013-06-17 10:43:42 -0600

    Fix running of all migrations from scratch.

--- knet/teachers/migrations/0001_initial.py
+++ knet/teachers/migrations/0001_initial.py
@@ -7,6 +7,8 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = [('accounts', '0001_initial')]
+
     def forwards(self, orm):
         # Adding model 'TeacherProfile'
         db.create_table('teachers_teacherprofile', (
