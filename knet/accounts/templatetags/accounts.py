ef85cf61f19b46b28a611289735f0287bfa2b2d8
Carl Meyer <carl@oddbird.net>
2013-06-19 11:59:17 -0400

    Properly HTML-escape OAuth provider login URL.

--- knet/accounts/templatetags/accounts.py
+++ knet/accounts/templatetags/accounts.py
@@ -1,5 +1,6 @@
 from django.middleware.csrf import get_token
 from django import template
+from django.utils.html import escape
 
 from .. import oauth
 
@@ -12,4 +13,4 @@ def oauth_authorize_url(request, redirect_to):
     """Get OAuth provider login url that will redirect to given URL."""
     provider = oauth.get_provider(
         redirect_to=redirect_to, state=get_token(request))
-    return provider.get_authorize_url()
+    return escape(provider.get_authorize_url())
