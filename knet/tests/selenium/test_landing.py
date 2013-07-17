e42361719110217c9990957a040c2be6012e4e13
Jonny Gerig Meyer <jonny@oddbird.net>
2013-07-17 16:58:48 -0400

    fix jstests; remove old selenium test

--- knet/tests/selenium/test_landing.py
+++ knet/tests/selenium/test_landing.py
@@ -2,13 +2,3 @@
 from selenium.webdriver.common.by import By
 
 from .pages import landing
-
-
-
-def test_user_can_submit_email_address(selenium):
-    landing_pg = landing.LandingPage(selenium)
-
-    landing_pg.go_to_landing_page()
-    landing_pg.signup()
-
-    assert landing_pg.is_element_present(By.CSS_SELECTOR, '#messages .success')
